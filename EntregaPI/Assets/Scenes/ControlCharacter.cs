9576835a8c3c54c7f510b7171e77fb4b161931c3
Darkai1 <hernandosc19@hotmail.com>
2022-03-17 23:28:13 -0500

    Script

--- EntregaPI/Assets/Scenes/ControlCharacter.cs
+++ EntregaPI/Assets/Scenes/ControlCharacter.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlCharacter : MonoBehaviour
-{
+public class ControlCharacter : MonoBehaviour {
+    public GameObject characterPrincipal;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
