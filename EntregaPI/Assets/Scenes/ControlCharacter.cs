3d66159626a0c5cfaaa00f33af71b9edb994c958
Darkai1 <hernandosc19@hotmail.com>
2022-03-18 00:00:27 -0500

    Script de movimiento
    Movimiento con flechas y wasd

--- EntregaPI/Assets/Scenes/ControlCharacter.cs
+++ EntregaPI/Assets/Scenes/ControlCharacter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ControlCharacter : MonoBehaviour {
     public GameObject characterPrincipal;
+    public float velocidad=20f;
     // Start is called before the first frame update
     void Start() {
         
@@ -11,6 +12,29 @@ public class ControlCharacter : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        if(Input.GetKey(KeyCode.W)){
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            transform.position -= Vector3.forward * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            transform.eulerAngles += new Vector3(0,velocidad* Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.eulerAngles -= new Vector3(0,velocidad* Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            transform.position -= Vector3.forward * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.eulerAngles += new Vector3(0,velocidad* Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.eulerAngles -= new Vector3(0,velocidad* Time.deltaTime);
+        }
     }
 }
